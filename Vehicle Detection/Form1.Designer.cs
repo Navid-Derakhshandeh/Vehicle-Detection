b4a149ebb79deb7d6b16a46749a296b2eeb12da9
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2023-02-14 19:33:55 +0330

    Traffic Control

---
+++ Vehicle Detection/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Vehicle_Detection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Disconnect = new System.Windows.Forms.Button();
+            this.Connect = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(57, 519);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(515, 290);
+            this.listBox1.TabIndex = 15;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.textBox1.Location = new System.Drawing.Point(324, 486);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.label1.Location = new System.Drawing.Point(53, 489);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 22);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Number Of Cars Have Counted:";
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(714, 4);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(709, 450);
+            this.videoSourcePlayer1.TabIndex = 12;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(707, 450);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Disconnect
+            // 
+            this.Disconnect.BackColor = System.Drawing.Color.Red;
+            this.Disconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.Disconnect.Location = new System.Drawing.Point(1005, 546);
+            this.Disconnect.Name = "Disconnect";
+            this.Disconnect.Size = new System.Drawing.Size(150, 51);
+            this.Disconnect.TabIndex = 10;
+            this.Disconnect.Text = "Disconnect";
+            this.Disconnect.UseVisualStyleBackColor = false;
+            this.Disconnect.Click += new System.EventHandler(this.Disconnect_Click);
+            // 
+            // Connect
+            // 
+            this.Connect.BackColor = System.Drawing.Color.Lime;
+            this.Connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.Connect.Location = new System.Drawing.Point(1005, 489);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(150, 51);
+            this.Connect.TabIndex = 9;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = false;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1425, 813);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Disconnect);
+            this.Controls.Add(this.Connect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vehicle Detection";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Disconnect;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Vehicle Detection/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Vision.Motion;
+using AForge.Imaging;
+
+namespace Vehicle_Detection
+{
+    public partial class Form1 : Form
+    {
+        MJPEGStream Video1;
+        MJPEGStream Video2;
+        MotionDetector motion;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Video1 = new MJPEGStream(""); //http://IP-Address/mjpg/video.mjpg
+            Video1.NewFrame += Frame1;
+            Video2 = new MJPEGStream("");  //http://IP-Address/mjpg/video.mjpg
+            //Video2 = new MJPEGStream(""); //http://IP-Address/mjpg/video.mjpg
+            motion = new MotionDetector(new TwoFramesDifferenceDetector(), new GridMotionAreaProcessing());
+        }
+        Double c;
+        private void Frame1(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bmp;
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            videoSourcePlayer1.VideoSource = Video2;
+            videoSourcePlayer1.Start();
+            Video1.Start();
+        }
+
+        private void Disconnect_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Video1.Stop();
+            videoSourcePlayer1.Stop();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+            videoSourcePlayer1.Stop();
+            Video1.Stop();
+        }
+
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            c = motion.ProcessFrame(image);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = c.ToString();
+            listBox1.Items.Add($"Number of Cars Hvae Counted:  +  {c.ToString()}");
+        }
+    }
+}
